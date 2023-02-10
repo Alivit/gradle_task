821e4d37e0426a2f950ae168f075e041f122c89a
Alivit <vltal241201@gmail.com>
2023-02-11 01:09:59 +0300

    fix: add check in Utils and conficuragion jar

--- core/src/main/java/ru/clevertec/Utils.java
+++ core/src/main/java/ru/clevertec/Utils.java
@@ -4,7 +4,10 @@ package ru.clevertec;
 import java.util.stream.Stream;
 
 public class Utils {
-    boolean isAllPositiveNumbers(String... str){
-        return Stream.of(str).allMatch(StringUtils::isPositiveNumber);
+    static boolean isAllPositiveNumbers(String... str){
+        if ( Stream.of(str).count() != 0 )
+            return Stream.of(str).allMatch(StringUtils::isPositiveNumber);
+        else return false;
+
     }
 }
